90e1dfc18e737a1d0fde96fe1853bfc3980cad33
Marc Valdez <127009880+marc-valdez@users.noreply.github.com>
2025-05-05 01:35:27 +0800

    Updated CustomerType buttons
    Added react-icons

--- resources/js/Pages/QueueForm/CustomerTypeStep.jsx
+++ resources/js/Pages/QueueForm/CustomerTypeStep.jsx
@@ -1,6 +1,9 @@
 import PrimaryButton from '@/Components/PrimaryButton';
 import SecondaryButton from '@/Components/SecondaryButton';
 import { Head } from '@inertiajs/react';
+import { FaUsers, FaWheelchair } from 'react-icons/fa';
+import { FaPersonCane } from 'react-icons/fa6';
+import { MdOutlinePregnantWoman } from 'react-icons/md';
 
 const CustomerTypeStep = ({ formData, onSelect, onNextStep, onPrevStep }) => {
   return (
@@ -12,20 +15,28 @@ const CustomerTypeStep = ({ formData, onSelect, onNextStep, onPrevStep }) => {
       <div className='mb-10 flex justify-center gap-8'>
         <SecondaryButton
           onClick={() => onSelect('customerType', 'Priority')}
-          className={`px-8 py-12 text-xl font-bold transition-all duration-200 ${
-            formData.customerType === 'Priority' ? 'scale-105 transform bg-yellow-600 text-white hover:bg-green-700' : 'bg-white text-gray-700 hover:bg-gray-100'
+          className={`w-96 h-48 px-12 py-10 text-xl font-bold transition-all duration-300 rounded-lg shadow-md flex flex-col items-center justify-center gap-4 ${
+            formData.customerType === 'Priority' ? 'bg-gradient-to-br from-amber-400 to-orange-500 text-white border-transparent' : 'bg-white text-gray-800 border border-gray-300 hover:bg-gray-100'
           }`}
         >
-          Priority Customer
+          <div className='flex gap-3 text-7xl'>
+            <FaWheelchair />
+            <FaPersonCane />
+            <MdOutlinePregnantWoman />
+          </div>
+          <span>Priority</span>
         </SecondaryButton>
 
         <SecondaryButton
           onClick={() => onSelect('customerType', 'Regular')}
-          className={`px-8 py-12 text-xl font-bold transition-all duration-200 ${
-            formData.customerType === 'Regular' ? 'scale-105 transform bg-blue-600 text-white hover:bg-green-700' : 'bg-white text-gray-700 hover:bg-gray-100'
+          className={`w-96 h-48 px-12 py-10 text-xl font-bold transition-all duration-300 rounded-lg shadow-md flex flex-col items-center justify-center gap-4 ${
+            formData.customerType === 'Regular' ? 'bg-gradient-to-br from-sky-400 to-cyan-500 text-white border-transparent' : 'bg-white text-gray-800 border border-gray-300 hover:bg-gray-100'
           }`}
         >
-          Regular Customer
+          <div className='text-7xl'>
+            <FaUsers />
+          </div>
+          <span>Regular</span>
         </SecondaryButton>
       </div>
       <div className='flex justify-center gap-6'>
