f0d0833e8cec0cee8945a1ccb4dd21a9e43c1e4e
Marc Valdez <127009880+marc-valdez@users.noreply.github.com>
2025-05-05 21:28:26 +0800

    Updated Application Logo on Login Layout

--- resources/js/Components/ApplicationLogo.jsx
+++ resources/js/Components/ApplicationLogo.jsx
@@ -8,5 +8,10 @@ export default function ApplicationLogo(props) {
         border='0'
       />
     </a>
+    // <img
+    //   src='https://i.pinimg.com/736x/b0/1d/a1/b01da1459e9c98b05f0458aeecc6a87f.jpg'
+    //   alt='QGo Company Logo'
+    //   className='h-16'
+    // />
   );
 }
